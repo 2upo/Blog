dd5078180b262dbc5dda461775def4044587b2c3
2upo <vladvodopad26@gmail.com>
2022-05-18 14:53:12 +0300

    JWT

--- src/router/router.go
+++ src/router/router.go
@@ -1,8 +1,22 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"blog/middleware"
+
+	"github.com/gin-gonic/gin"
+)
 
 func InitRoutes(baseGroup *gin.RouterGroup) {
+
+	authMiddleware := middleware.InitJWT()
+
 	baseGroup.GET("healthcheck", healthcheck)
-	baseGroup.GET("")
-} 
+	baseGroup.POST("/login", authMiddleware.LoginHandler)
+	auth := baseGroup.Group("/auth")
+	// Refresh time can be longer than token timeout
+	auth.GET("/refresh_token", authMiddleware.RefreshHandler)
+	auth.Use(authMiddleware.MiddlewareFunc())
+	{
+		auth.GET("/hello", helloHandler)
+	}
+}
--- src/user/model.go
+++ src/user/model.go
@@ -1,1 +1,14 @@
 package user
+
+type User struct {
+	UserName  string
+	FirstName string
+	LastName  string
+}
+
+type Login struct {
+	Username string `form:"username" json:"username" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
+}
+
+var IdentityKey = "id"
